b7644d0e1a817230d024f69156f8ed394d03f3c1
Win Aung <91712429+winaung1@users.noreply.github.com>
2023-08-25 21:16:40 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>This is Win Aung's PHP page</h1>
+    <p>Someday, this page will be an awesome page!</p>
+    <a href="index.php">This link will help Jeff grade this assignment</a> :0]
+</body>
+</html>
